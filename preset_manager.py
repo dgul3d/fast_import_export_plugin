7cc99a301de35b8dfb96925560634583773ba636
DG <64034875+dguliev-github@users.noreply.github.com>
2024-08-18 20:27:26 +0300

    Add import_types parse capability

--- preset_manager.py
+++ preset_manager.py
@@ -9,10 +9,19 @@ export_types={"better_export.fbx": "Better Fbx: ",
               "wm.ply_export": "Ply: ",
               "wm.usd_export": "USD: "}
 
+import_types={"better_import.fbx": "Better Fbx: ",
+              "import_scene.fbx" : "Fbx: ",
+              "import_scene.gltf": "Gltf: ",
+              "wm.obj_import": "Obj: ",
+              "wm.ply_import": "Ply: ",
+              "wm.usd_import": "USD: "}
+
 def get_operator_presets(preset_type = 'export_scene.fbx'):
     preset_operator = 'bpy.ops.'+preset_type
     preset_type = 'operator\\'+preset_type
     preset_list = [] 
+    if bpy.utils.preset_paths(preset_type) == []:
+        return
     parent_path = bpy.utils.preset_paths(preset_type)[0]
     presets = os.listdir(parent_path)
     for preset in presets:
@@ -27,7 +36,9 @@ def get_operator_presets(preset_type = 'export_scene.fbx'):
             with open(preset_filepath, 'r') as file:
                 # storing the values from the preset on the class
                 for line in file.readlines()[3::]:
-                    exec(line, globals(), locals())
+                    # import presets contain garbage lines
+                    if "=" in line and line.startswith("op."):
+                        exec(line, globals(), locals())
 
                 # pass class dictionary to the operator
                 kwargs = op.__dict__
@@ -35,20 +46,26 @@ def get_operator_presets(preset_type = 'export_scene.fbx'):
             preset_list.append({"preset_name" : preset_name, "preset_parms": {"preset_operator" : preset_operator,"kwargs": kwargs}})
     return preset_list
 
-def get_preset_tree():
+def get_preset_tree(is_export=True):
     preset_tree = {}
-    for export_type in export_types:
-        preset_list = get_operator_presets(export_type) 
-        for preset in preset_list: 
-            preset["preset_name"] = export_types[export_type]+preset["preset_name"]
-            preset_tree[preset["preset_name"]] = preset["preset_parms"]
+    if is_export:
+        preset_types = export_types
+    else:
+        preset_types = import_types
+    for preset_type in preset_types:
+        preset_list = get_operator_presets(preset_type)
+        if preset_list:
+            for preset in preset_list: 
+                preset["preset_name"] = preset_types[preset_type]+preset["preset_name"]
+                preset_tree[preset["preset_name"]] = preset["preset_parms"]
     return preset_tree
 
-def get_preset_items():
-    list_of_preset_names = list(get_preset_tree().keys())
+def get_preset_items(is_export=True):
+    list_of_preset_names = list(get_preset_tree(is_export).keys())
     preset_items = [(name,name,name) for name in list_of_preset_names]
     return preset_items
 
 if __name__ == "__main__":
     export_preset = "Fbx: FBX_Default"
-    print(get_preset_tree()[export_preset]["preset_operator"])
+    #print(get_preset_tree(is_export=False))
+    print(get_preset_tree(True))
